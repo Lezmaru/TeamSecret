66b05e5ddd1bb692b4735ec39cd44b85d585ae4e
Lezmaru <luis.perez@ucb.edu.bo>
2023-02-24 00:24:03 -0400

    Avance capa Login

--- lib/login.dart
+++ lib/login.dart
@@ -13,10 +13,80 @@ class _LoginState extends State<Login> {
       child: Scaffold(
         body: Center(
           child: Column(
-            children: [Image.asset(name)],
+            children: [
+              Image.asset(
+                'images/logo.png',
+                height: 200.0,
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              _userTextField(),
+              SizedBox(
+                height: 20,
+              ),
+              _passwordTextField(),
+              SizedBox(
+                height: 20,
+              ),
+              _bottonLogin(),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  Widget _userTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            icon: Icon(Icons.lock),
+            hintText: 'ejemplo@correo.com',
+            labelText: 'Correo electrónico',
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget _passwordTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: TextField(
+          obscureText: true,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            icon: Icon(Icons.lock),
+            hintText: 'Contraseña',
+            labelText: 'Contraseña',
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget _bottonLogin() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return RaisedButton(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+            child: Text('Ingresar'),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          onPressed: () {});
+    });
   }
 }
